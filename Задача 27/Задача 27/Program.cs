90f59457e5cefe126be0cec2eae5b7465b5321ec
lildiamond <kolov.efimka@gmail.com>
2023-01-22 19:47:01 +1000

    work

---
+++ Задача 27/Задача 27/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Напишите программу, которая задаёт массив из 8 элементов и
+//выводит их на экран. (числа берете любые)
+
+
+
+void massiv()
+{
+    int[] mass = new int[8];
+    Random rand = new Random();
+
+    for (int i = 0; i < 8; i++)
+    {
+        mass[i] = rand.Next(100000);
+    }
+
+    Console.Write("Ваш массив:");
+
+    for (int j = 0; j < 8; j++)
+    {
+        Console.Write(mass[j]);
+        Console.Write(" ");
+    }
+};
+
+massiv();
---
+++ Задача26/Задача26/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+using System.Diagnostics.Metrics;
+
+void task()
+{
+    Console.WriteLine("Введите число:");
+    int X = Int32.Parse(Console.ReadLine()!);
+    int buff1 = GetQuantity(X);
+    Console.WriteLine($"Сумма чисел введенного числа: {power(X, buff1)}");
+}
+
+int GetQuantity(int A)
+{
+    int Quantity = 0;
+    while (A > 0)
+    { Quantity = Quantity + 1; A = A / 10; }
+    return Quantity;
+}
+
+
+
+int power(int B, int buff1)
+{
+
+    int advance = 0;
+    int result = 0;
+    for (int i = 0; i < buff1; i++)
+    {
+        advance = B - B % 10;
+        result = result + (B - advance);
+        B = B / 10;
+    }
+    return result;
+
+}
+
+task();
+
+
