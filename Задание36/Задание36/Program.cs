48a1b4164e208a7560f46814dd67ede0ad2aba53
lil_diamond <45915050+gitdiamond@users.noreply.github.com>
2023-01-25 12:30:55 +1000

    Add files via upload

---
+++ Задание36/Задание36/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму элементов, стоящих на нечётных индексах.
+
+
+
+
+
+void massiv()
+{
+    int[] mass = new int[10];
+    int count = 0;
+    Random rand = new Random();
+
+    for (int i = 0; i < 10; i++)
+    {
+        mass[i] = rand.Next(0, 10);
+        if (i % 2 == 1) count = count + mass[i];
+    }
+
+    Console.Write("Ваш массив:");
+
+    for (int j = 0; j < 10; j++)
+    {
+        Console.Write(mass[j]);
+        Console.Write(" ");
+
+    }
+    Console.WriteLine();
+    Console.Write($"Сумма элементов, стоящих на нечётных индексах: {count}");
+
+};
+
+massiv();
---
+++ Задача34/Задача34/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+
+
+void massiv()
+{
+    int[] mass = new int[10];
+    int count = 0;
+    Random rand = new Random();
+
+    for (int i = 0; i < 10; i++)
+    {
+        mass[i] = rand.Next(100, 1000);
+        if (mass[i] % 2 == 0) count++;
+    }
+
+    Console.Write("Ваш массив:");
+
+    for (int j = 0; j < 10; j++)
+    {
+        Console.Write(mass[j]);
+        Console.Write(" ");
+        Console.Write(" ");
+    }
+    Console.Write($"Количество чётных чисел: {count}");
+
+};
+
+massiv();
