752cc66a3fed9c6fb658ae8316ead29b6ebc7f58
lil_diamond <45915050+gitdiamond@users.noreply.github.com>
2023-01-31 13:03:12 +1000

    Add files via upload

---
+++ Задание43/Задание43/Program.cs
@@ -0,0 +1,47 @@
+﻿
+Console.WriteLine("Генерация массива:");
+int[] mainSort = Gen();
+Sort(mainSort);
+print(mainSort);
+
+int[] Gen()
+{
+    int value = new Random().Next(5, 10);
+    int[] massiv = new int[value];
+    for (int i = 0; i < value; i++)
+    {
+        massiv[i] = new Random().Next(0, 100);
+        Console.Write(massiv[i]);
+        Console.Write(" ");
+    }
+    return massiv;
+}
+//
+void Sort(int[] array)
+{
+    int buff;
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[i] > array[j])
+            {
+                buff = array[i];
+                array[i] = array[j];
+                array[j] = buff;
+            }
+        }
+    }
+
+}
+
+void print(int[] array)
+{
+    Console.WriteLine();
+    Console.WriteLine("Отсортированный массив:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(" ");
+    }
+}
