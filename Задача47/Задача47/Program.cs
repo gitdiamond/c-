1ea28bf3f383df0677fa8d3e972eaa72dae0a798
lil_diamond <45915050+gitdiamond@users.noreply.github.com>
2023-02-03 00:38:19 +1000

    Add files via upload

---
+++ Задача47/Задача47/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Генерация массива:");
+Console.WriteLine("Введите количество строк и столбцов массива:");
+int firstt = Convert.ToInt32(Console.ReadLine());
+int secondd = Convert.ToInt32(Console.ReadLine());
+double[,] mainSort = Gen(firstt, secondd); ;
+
+double[,] Gen(int first, int second)
+{
+    double[,] massiv = new double[first, second];
+    for (int i = 0; i < first; i++)
+    {
+        for (int j = 0; j < second; j++)
+        {
+            massiv[i, j] = new Random().NextDouble() * 10;
+            Console.Write($"{massiv[i, j]:f1}");
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+    return massiv;
+}
+
+
